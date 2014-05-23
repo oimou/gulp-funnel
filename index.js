69ddbb42282bd5dcfe3b3c57124f54782729f5d7
Keiji Yahata <yahata@nowhere.co.jp>
2014-05-23 16:24:27 +0900

    plugin: extract key-value table from source file

--- index.js
+++ index.js
@@ -1,25 +1,61 @@
-var through = require("through2");
+var cheerio = require("cheerio");
+var through2 = require("through2");
 var gutil = require("gulp-util");
 var PluginError = gutil.PluginError;
 
 const PLUGIN_NAME = "gulp-extract-text";
 
-function gulpExtractText(html) {
-  var stream = through.obj(function (file, enc, callback) {
-    if (file.isNull()) {
-    }
+function gulpExtractText(opt) {
+  var attr, dest;
 
-    if (file.isBuffer()) {
+  opt = opt || {};
+  attr = opt.attr || "data-et-tag";
+  dest = (opt.dest) ? opt.dest : [];
+  dest = (dest instanceof Array) ? dest : [dest];
+
+  function transform(file, encode, callback) {
+    if (file.isNull()) {
+      this.push(file);
+      return callback();
     }
 
     if (file.isStream()) {
+      this.emit("error", new gutil.PluginError("gulp-diff", "Streaming not supported"));
+      return callback();
     }
 
-    this.push(file);
-    return callback();
-  });
+    var content = file.contents.toString();
+    var $ = cheerio.load(content);
 
-  return stream;
+    //
+    //  table
+    //
+    var table = {};
+
+    //
+    //  extract values by tags
+    //
+    $("*")
+      .filter(function () {
+        return !!$(this).attr(attr);
+      })
+      .map(function () {
+        var tag = $(this).attr(attr);
+        var text = $(this).text();
+
+        table[tag] = text;
+      }).get();
+
+    return callback();
+  }
+
+  function flush(callback) {
+    callback();
+  }
+
+  var th2 = through2.obj(transform, flush);
+
+  return th2;
 }
 
 module.exports = gulpExtractText;
